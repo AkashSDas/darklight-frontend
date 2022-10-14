463bea80f638bf920831b73089698c41aa91b255
AkashSDas <aakashdas368@gmail.com>
2022-10-14 12:24:42 +0530

    add signup logic

--- components/signup/basic.tsx
+++ components/signup/basic.tsx
@@ -1,21 +1,147 @@
+import { useFormik } from "formik";
+import debounce from "lodash.debounce";
+import { useCallback, useEffect } from "react";
+import { ISignupPayload } from "services/auth";
+
 import FlatButton from "@components/buttons/flat-button";
 import FormLabel from "@components/form/label";
+import { useAppDispatch, useAppSelector } from "@hooks/store";
+import { signupValidation } from "@lib/validations";
+import { signupThunk } from "@store/auth/thunk";
+import { emailAvailabilityCheckThunk, usernameAvailabilityCheckThunk } from "@store/user/thunk";
 import styles from "@styles/components/signup/basic.module.scss";
 
 function BasicSignup() {
+  var dispatch = useAppDispatch();
+  var { signupLoading } = useAppSelector((state) => state.auth);
+  var {
+    checkingEmailAvailable,
+    checkingUsernameAvailable,
+    isEmailAvailable,
+    isUsernameAvailable,
+  } = useAppSelector((state) => state.user);
+
+  // ===============================================
+  // Formik settings
+  // ===============================================
+
+  var initialValues: ISignupPayload = {
+    fullName: "",
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+
+  async function handleSubmit(values: ISignupPayload) {
+    await dispatch(signupThunk(values));
+  }
+
+  var formik = useFormik({
+    initialValues,
+    onSubmit: handleSubmit,
+    validationSchema: signupValidation,
+  });
+
+  var { handleBlur, handleChange, values: formValues } = formik;
+
+  // ===============================================
+  // Check username and email availability
+  // ===============================================
+
+  var checkUsernameAvailability = useCallback(
+    debounce(async function checkAvailability(username: string) {
+      if (username.length >= 2 && !formik.errors.username) {
+        await dispatch(usernameAvailabilityCheckThunk(username));
+      }
+    }, 500),
+    []
+  );
+
+  var checkEmailAvailability = useCallback(
+    debounce(async function checkAvailability(email: string) {
+      if (email.length > 0 && !formik.errors.email) {
+        await dispatch(emailAvailabilityCheckThunk(email));
+      }
+    }, 500),
+    []
+  );
+
+  useEffect(() => {
+    checkUsernameAvailability(formik.values.username);
+  }, [formik.values.username, checkUsernameAvailability]);
+
+  useEffect(() => {
+    checkEmailAvailability(formik.values.email);
+  }, [formik.values.email, checkEmailAvailability]);
+
+  // ===============================================
+  // JSX
+  // ===============================================
+
+  function UsernameAvailabilityMsg() {
+    if (checkingUsernameAvailable) {
+      return (
+        <span className={`${styles.info_msg} -text-cap`}>Checking...</span>
+      );
+    } else if (isUsernameAvailable) {
+      return (
+        <span className={`${styles.success_msg} -text-cap`}>Available</span>
+      );
+    }
+    return (
+      <span className={`${styles.error_msg} -text-cap`}>Already used</span>
+    );
+  }
+
+  function EmailAvailabilityMsg() {
+    if (checkingEmailAvailable) {
+      return (
+        <span className={`${styles.info_msg} -text-cap`}>Checking...</span>
+      );
+    } else if (isEmailAvailable) {
+      return (
+        <span className={`${styles.success_msg} -text-cap`}>Available</span>
+      );
+    }
+    return (
+      <span className={`${styles.error_msg} -text-cap`}>Already used</span>
+    );
+  }
+
+  function InputValidationMsg({ name }: { name: string }) {
+    var { username: u_error, email: e_error } = formik.errors;
+    var { username: u_value, email: e_value } = formik.values;
+
+    if (name == "username" && u_value.length > 0 && !u_error) {
+      return <UsernameAvailabilityMsg />;
+    } else if (name == "email" && e_value.length > 0 && !e_error) {
+      return <EmailAvailabilityMsg />;
+    }
+
+    return (
+      <span className={`${styles.error_msg} -text-cap`}>
+        {formik.touched[name] && formik.errors[name]}
+      </span>
+    );
+  }
+
   return (
-    <form className={styles.container}>
+    <form onSubmit={formik.handleSubmit} className={styles.container}>
       {/* ================= Fullname and username ================= */}
       <div className={styles.inline_group}>
         <div className={styles.input_group}>
-          <FormLabel label="Fullname" htmlFor="fullname" />
+          <FormLabel label="Fullname" htmlFor="fullName" />
           <input
-            id="fullname"
-            name="fullname"
+            id="fullName"
+            name="fullName"
             type="text"
             autoComplete="off"
-            required
+            value={formValues.fullName}
+            onChange={handleChange}
+            onBlur={handleBlur}
           />
+          <InputValidationMsg name="fullName" />
         </div>
 
         <div className={styles.input_group}>
@@ -25,8 +151,11 @@ function BasicSignup() {
             name="username"
             type="text"
             autoComplete="off"
-            required
+            value={formValues.username}
+            onChange={handleChange}
+            onBlur={handleBlur}
           />
+          <InputValidationMsg name="username" />
         </div>
       </div>
 
@@ -38,8 +167,11 @@ function BasicSignup() {
           name="email"
           type="email"
           autoComplete="off"
-          required
+          value={formValues.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
+        <InputValidationMsg name="email" />
       </div>
 
       {/* ================= Password and confirm password ================= */}
@@ -52,8 +184,11 @@ function BasicSignup() {
             type="password"
             placeholder="Minimum 6 characters"
             autoComplete="off"
-            required
+            value={formValues.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
           />
+          <InputValidationMsg name="password" />
         </div>
 
         <div className={styles.input_group}>
@@ -63,12 +198,19 @@ function BasicSignup() {
             name="confirmPassword"
             type="password"
             autoComplete="off"
-            required
+            value={formValues.confirmPassword}
+            onChange={handleChange}
+            onBlur={handleBlur}
           />
+          <InputValidationMsg name="confirmPassword" />
         </div>
       </div>
 
-      <FlatButton size="lg" label="Signup with email" />
+      <FlatButton
+        size="lg"
+        label={signupLoading ? "Signing you up..." : "Signup with email"}
+        disabled={signupLoading}
+      />
     </form>
   );
 }
---
+++ lib/validations.ts
@@ -0,0 +1,21 @@
+import * as Yup from "yup";
+
+export var signupValidation = Yup.object({
+  fullName: Yup.string()
+    .required("Full name is required")
+    .min(6, "Fullname is too short")
+    .max(240, "Fullname is too long"),
+  username: Yup.string()
+    .required("Username is required")
+    .min(3, "Username is too short")
+    .max(120, "Username is too long"),
+  email: Yup.string()
+    .required("Email is required")
+    .email("Invalid email address"),
+  password: Yup.string()
+    .required("Password is required")
+    .min(6, "Password is too short"),
+  confirmPassword: Yup.string()
+    .required("Confirm password is required")
+    .oneOf([Yup.ref("password"), null], "Passwords must match"),
+});

dcbabf519d6f063c3511527435bf1fee765c9f2e
AkashSDas <aakashdas368@gmail.com>
2022-10-15 06:54:52 +0530

    add helper element

--- components/form/label.tsx
+++ components/form/label.tsx
@@ -1,7 +1,19 @@
-function FormLabel({ label, htmlFor }: { label: string; htmlFor: string }) {
+import { ReactElement } from "react";
+
+interface Props {
+  label: string;
+  htmlFor: string;
+  rightElement?: ReactElement;
+}
+
+function FormLabel({ label, htmlFor, rightElement }: Props) {
   return (
-    <label htmlFor={htmlFor} className="-text-body font-medium text-grey7">
-      {label}
+    <label
+      htmlFor={htmlFor}
+      className="-text-body font-medium flex items-center justify-between"
+    >
+      <span className="text-grey7">{label}</span>
+      {rightElement}
     </label>
   );
 }

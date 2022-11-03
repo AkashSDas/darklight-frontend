d1b35f40118775a4f4b55f60183d6da6a0749083
AkashSDas <aakashdas368@gmail.com>
2022-11-03 20:46:08 +0530

    create services for modules and lessons

--- services/_course.ts
+++ services/_course.ts
@@ -1,6 +1,8 @@
 import { AxiosRequestConfig } from "axios";
 import fetchAPI from "services";
 
+import { Module } from "@store/_course/slice";
+
 function fetchFromCourse(endpoint: string, opts: AxiosRequestConfig) {
   const BASE_URL = "/course";
   return fetchAPI(`${BASE_URL}/${endpoint}`, opts);
@@ -40,4 +42,69 @@ export async function updateCourseInfoService(
   });
 
   return { success: res.status < 300, msg: res.msg, course: res.data?.course };
+}
+
+export async function getModuleService(courseId: string, moduleId: string) {
+  var res = await fetchFromCourse(`${courseId}/${moduleId}`, { method: "get" });
+  return res.data;
+}
+
+export async function createModuleService(token: string, courseId: string) {
+  var res = await fetchFromCourse(courseId, {
+    method: "post",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  return { success: res.status < 300, msg: res.msg, course: res.data };
+}
+
+export async function updateModuleService(
+  token: string,
+  courseId: string,
+  moduleId: string,
+  data: Module
+) {
+  var res = await fetchFromCourse(`${courseId}/${moduleId}`, {
+    method: "put",
+    headers: { Authorization: `Bearer ${token}` },
+    data,
+  });
+  return { success: res.status < 300, msg: res.msg, module: res.data };
+}
+
+export async function reorderModules(
+  token: string,
+  courseId: string,
+  data: Module[]
+) {
+  var res = await fetchFromCourse(`${courseId}/reorder`, {
+    method: "put",
+    headers: { Authorization: `Bearer ${token}` },
+    data,
+  });
+  return { success: res.status < 300, msg: res.msg, course: res.data };
+}
+
+export async function createLessonService(
+  token: string,
+  courseId: string,
+  moduleId: string
+) {
+  var res = await fetchFromCourse(`${courseId}/${moduleId}`, {
+    method: "post",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  return { success: res.status < 300, msg: res.msg, lesson: res.data };
+}
+
+export async function getLessonService(
+  courseId: string,
+  moduleId: string,
+  lessonId: string
+) {
+  var res = await fetchFromCourse(`${courseId}/${moduleId}/${lessonId}`, {
+    method: "get",
+  });
+  return res.data;
 }

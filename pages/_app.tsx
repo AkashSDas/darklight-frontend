29fb0e6a222656dbb92f70d2f160e7641125307f
AkashSDas <aakashdas368@gmail.com>
2022-12-02 06:54:32 +0530

    setup design system

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Urbanist } from "@next/font/google";
+import localFont from "@next/font/local";
+
+const urbanist = Urbanist({ variable: "--font-urbanist" });
+const gilroy = localFont({
+  src: "../public/fonts/gilroy-extrabold.otf",
+  variable: "--font-gilroy",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${urbanist.variable} ${gilroy.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        urbanist: ["var(--font-urbanist)"],
+        gilroy: ["var(--font-gilroy)"],
+      },
+      colors: {
+        background1: "#FFFFFF",
+        background2: "#FAFAFA",
+        background3: "#F5F5F5",
+        border: "#F0F0F0",
+        text1: "#1A1B22",
+        text2: "#8B8B8B",
+        text3: "#FFFFFF",
+        primary: "#3A4EFF",
+        "light-primary": "#6676FF",
+        success: "#34A853",
+        "light-success": "#F3FFD9",
+        error: "#EA4335",
+        "light-error": "#FFECEB",
+        link: "#4285F4",
+      },
+    },
   },
   plugins: [],
 };

6b68c68f1bfb9ba65fdc9f41fdde40e71221b081
AkashSDas <aakashdas368@gmail.com>
2022-12-05 15:04:50 +0530

    integrate swr and create use user hook

--- lib/hooks.lib.ts
+++ lib/hooks.lib.ts
@@ -1,7 +1,20 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import useSWR from "swr";
 
+import { getNewAccessToken } from "../services/auth.service";
 import { AppDispatch, RootState } from "../store";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export var useAppDispatch = () => useDispatch<AppDispatch>();
 export var useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export function useUser() {
+  var { data, error } = useSWR("access-token", getNewAccessToken, {});
+
+  return {
+    success: data?.success,
+    user: data?.user,
+    accessToken: data?.accessToken,
+    error: error,
+  };
+}

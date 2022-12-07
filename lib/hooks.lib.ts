a08d7b09c458effca0d124892a5984a626cf0cbb
AkashSDas <aakashdas368@gmail.com>
2022-12-07 17:06:06 +0530

    add dropdown hooks

--- lib/hooks.lib.ts
+++ lib/hooks.lib.ts
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { useEffect, useRef } from "react";
+import { MutableRefObject, useEffect, useRef, useState } from "react";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import useSWR, { useSWRConfig } from "swr";
 
@@ -115,4 +115,46 @@ export function useResizeTextareaHeight(
   }, [value]);
 
   return { ref };
+}
+
+/**
+ * Hook that alerts clicks outside of the passed ref
+ */
+export function useOutsideAlerter(
+  ref: MutableRefObject<any>,
+  onOutsideClick: Function
+) {
+  useEffect(
+    function alertOnOutsideClick() {
+      function handleOutsideClick(event: MouseEvent) {
+        // Outside click
+        if (ref.current && !ref.current.contains(event.target)) {
+          onOutsideClick();
+        }
+      }
+
+      // Bind the event listener
+      document.addEventListener("mousedown", handleOutsideClick);
+
+      return function unbindEvent() {
+        // Unbind the event listener on clean up
+        document.removeEventListener("mousedown", handleOutsideClick);
+      };
+    },
+    [ref, onOutsideClick]
+  );
+}
+
+/**
+ * Hook for managing dropdown state (outside click, open/close)
+ */
+export function useDropdown() {
+  var wrapperRef = useRef(null);
+  var [isOpen, setIsOpen] = useState(false);
+
+  useOutsideAlerter(wrapperRef, function updateDropdownOnOutsideClick() {
+    setIsOpen(false);
+  });
+
+  return { wrapperRef, isOpen, setIsOpen };
 }

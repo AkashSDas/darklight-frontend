05c6218887fea139c365ce59aa9e5027aa344f4a
AkashSDas <aakashdas368@gmail.com>
2022-12-09 14:53:58 +0530

    add title input

--- pages/courses/[courseId]/groups/[groupId].tsx
+++ pages/courses/[courseId]/groups/[groupId].tsx
@@ -2,6 +2,7 @@ import { ReactElement } from "react";
 
 import CourseEditorLayout from "../../../../components/course-editor/layout";
 import GroupEmojiInput from "../../../../components/group-settings/emoji-input";
+import GroupTitleInput from "../../../../components/group-settings/title-input";
 import { useEditableGroup } from "../../../../lib/hooks.lib";
 
 export default function GroupPage() {
@@ -11,6 +12,7 @@ export default function GroupPage() {
     <main className="mt-4 w-full flex flex-col gap-2 items-center">
       <section className="w-full max-w-[800px] flex flex-col gap-2">
         <GroupEmojiInput />
+        <GroupTitleInput />
       </section>
     </main>
   );

9c1ac00ac073d16f5d3de2f8bd4dcf70f4199465
AkashSDas <aakashdas368@gmail.com>
2022-12-09 20:39:20 +0530

    refactor file

--- pages/courses/[courseId]/groups/[groupId]/lessons/[lessonId].tsx
+++ pages/courses/[courseId]/groups/[groupId]/lessons/[lessonId].tsx
@@ -1,6 +1,6 @@
 import { ReactElement } from "react";
 
-import CourseEditorLayout from "../../../../../components/course-editor/layout";
+import CourseEditorLayout from "../../../../../../components/course-editor/layout";
 
 export default function LessonPage() {
   return <div></div>;

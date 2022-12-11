260d7832b5d3ee88fb55b20f4f5ca2834100ba2f
AkashSDas <aakashdas368@gmail.com>
2022-12-11 07:27:05 +0530

    create image block

--- components/lesson-editor-settings/content-blocks.tsx
+++ components/lesson-editor-settings/content-blocks.tsx
@@ -1,5 +1,6 @@
 import { useEditableLesson } from "@lib/hooks.lib";
 
+import ImageBlock from "./image-block";
 import ParagraphBlock from "./paragraph-block";
 
 export default function ContentBlocks() {
@@ -8,10 +9,13 @@ export default function ContentBlocks() {
   //   return <div>{JSON.stringify(lesson.content)}</div>;
 
   return (
-    <div className="flex flex-col gap-2 mb-6">
+    <div className="flex flex-col gap-3 mb-6">
       {lesson?.content.map((block: any) => {
         if (block.type == "paragraph") {
           return <ParagraphBlock key={block.id} id={block.id} />;
+        }
+        if (block.type == "image") {
+          return <ImageBlock key={block.id} id={block.id} />;
         }
         return null;
       })}

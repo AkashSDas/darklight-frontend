87a96b4fb8ea67356b76a465094145b3c6f1eddb
AkashSDas <aakashdas368@gmail.com>
2022-12-11 06:34:33 +0530

    refactor

--- pages/courses/[courseId]/groups/[groupId]/lessons/[lessonId].tsx
+++ pages/courses/[courseId]/groups/[groupId]/lessons/[lessonId].tsx
@@ -2,6 +2,7 @@ import { ReactElement } from "react";
 
 import CourseEditorLayout from "@components/course-editor/layout";
 import { SearchIcon } from "@components/icons/search";
+import ContentBlocks from "@components/lesson-editor-settings/content-blocks";
 import LessonEmojiInput from "@components/lesson-editor-settings/emoji-input";
 import TitleInput from "@components/lesson-editor-settings/title-input";
 import ContentBlockDropdown from "@components/lesson-layout/content-block-dropdown";
@@ -9,7 +10,7 @@ import SearchBlockInput from "@components/lesson-layout/search-block-input";
 import { useEditableLesson } from "@lib/hooks.lib";
 
 export default function LessonPage() {
-  var { loading, lesson } = useEditableLesson();
+  var { loading } = useEditableLesson();
 
   if (loading) return <div>Loading...</div>;
 
@@ -19,8 +20,8 @@ export default function LessonPage() {
         <section className="w-full max-w-[840px] flex flex-col gap-2 px-16">
           <LessonEmojiInput />
           <TitleInput />
-
           <hr className="bg-border my-2 h-[1px]" />
+          <ContentBlocks />
         </section>
       </main>
 

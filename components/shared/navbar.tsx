6f89be09998d332a1dd556f9e577d28d26fc5d27
AkashSDas <aakashdas368@gmail.com>
2022-12-21 16:44:43 +0530

    refactor and add logout feature

--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -1,32 +1,12 @@
 import Link from "next/link";
 import { toast } from "react-hot-toast";
 import { logout } from "services/auth.service";
-import { useSWRConfig } from "swr";
 
+import { useUser } from "@lib/hooks.lib";
 import DarkLightLogo from "@public/logo.svg";
 
 export default function Navbar(): JSX.Element {
-  function VerticalLine(): JSX.Element {
-    return (
-      <div className="border-l border-solid border-l-border h-[22px]"></div>
-    );
-  }
-
-  function LoggedOutActions(): JSX.Element {
-    return (
-      <>
-        <Link href="/auth/login">
-          <button>Login</button>
-        </Link>
-
-        <Link href="/auth/login">
-          <button className="text-text3 bg-primary hover:bg-[#3446E5] active:bg-[#2E3ECC]">
-            Signup
-          </button>
-        </Link>
-      </>
-    );
-  }
+  var { user } = useUser();
 
   return (
     <nav className="px-8 w-full h-[70px] flex justify-between items-center">
@@ -46,26 +26,57 @@ export default function Navbar(): JSX.Element {
         </Link>
 
         <VerticalLine />
-        <LoggedOutActions />
-        <LoggedInActions />
+        {!user ? <LoggedOutActions /> : <LoggedInActions />}
       </div>
     </nav>
   );
 }
 
+function VerticalLine(): JSX.Element {
+  return <div className="border-l border-solid border-l-border h-[22px]"></div>;
+}
+
 function LoggedInActions(): JSX.Element {
-  var { mutate } = useSWRConfig();
+  var { user, accessToken, mutateAccessToken, mutateUser } = useUser();
 
   async function logoutUser() {
-    await logout();
-    await mutate("access-token", { success: null, accessToken: null });
-    await mutate("user", { success: null, user: null, error: null });
+    if (!user) return;
+
+    var userAuthType = accessToken ? "accessToken" : "oauth";
+    if (accessToken) {
+      await mutateAccessToken(
+        { success: true, accessToken: null, user: null },
+        { revalidate: false }
+      );
+    } else if (user) {
+      await mutateUser({ success: true, user: null }, { revalidate: false });
+    }
+
     toast.success("Logged out successfully");
+    await logout();
+    if (userAuthType == "accessToken") await mutateAccessToken();
+    else await mutateUser();
   }
 
   return (
     <>
       <button onClick={logoutUser}>Logout</button>
+    </>
+  );
+}
+
+function LoggedOutActions(): JSX.Element {
+  return (
+    <>
+      <Link href="/auth/login">
+        <button>Login</button>
+      </Link>
+
+      <Link href="/auth/login">
+        <button className="text-text3 bg-primary hover:bg-[#3446E5] active:bg-[#2E3ECC]">
+          Signup
+        </button>
+      </Link>
     </>
   );
 }

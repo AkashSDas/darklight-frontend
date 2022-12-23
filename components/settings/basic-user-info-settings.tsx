63256e0d105d04556fb9eef5ba833aa0298897ee
AkashSDas <aakashdas368@gmail.com>
2022-12-23 21:05:27 +0530

    refactor user info settings section

--- components/settings/basic-user-info-settings.tsx
+++ components/settings/basic-user-info-settings.tsx
@@ -1,16 +1,141 @@
+import { useFormik } from "formik";
 import Image from "next/image";
-import { ChangeEvent, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import toast from "react-hot-toast";
-import { updateProfileImage } from "services/user.service";
+import { updateDetails, updateProfileImage } from "services/user.service";
 
+import { FormLabel } from "@components/form/label";
 import { CameraIcon } from "@components/shared/icons";
 import { useUser } from "@lib/hooks.lib";
+import { UpdateDetailsInput } from "@lib/user.lib";
 
 export default function BasicUserInfoSettings(): JSX.Element {
   return (
     <div className="w-full flex gap-16 justify-center">
       <UpdateProfileImage />
+      <UpdateUserDetailsForm />
     </div>
+  );
+}
+
+function UpdateUserDetailsForm(): JSX.Element {
+  var { user, accessToken } = useUser();
+  var [loading, setLoading] = useState(false);
+
+  var formik = useFormik<UpdateDetailsInput>({
+    initialValues: {
+      fullName: user.fullName ?? "",
+      username: user.username,
+      email: user.email,
+    },
+    onSubmit: async function onSubmit(values) {
+      setLoading(true);
+      var { success } = await updateDetails(accessToken, values);
+      if (success) toast.success("Details updated successfully");
+      else toast.error("Error updating details");
+      setLoading(false);
+    },
+  });
+
+  var displayUsernameError = formik.touched.username && formik.errors.username;
+  var displayEmailError = formik.touched.email && formik.errors.email;
+  var displayFullnameError = formik.touched.fullName && formik.errors.fullName;
+
+  return (
+    <section className="w-full max-w-[360px]">
+      <form onSubmit={formik.handleSubmit} className="flex flex-col gap-[18px]">
+        {/* Fullname Input */}
+        <div className="flex flex-col gap-1">
+          <div className="flex items-center justify-between">
+            <FormLabel htmlFor="fullname" label="Fullname" variant="regular" />
+
+            {displayFullnameError && (
+              <FormLabel
+                label={
+                  displayFullnameError ? (formik.errors.fullName as string) : ""
+                }
+                variant="error"
+              />
+            )}
+          </div>
+
+          <input
+            required
+            type="text"
+            name="fullName"
+            value={formik.values.fullName}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className={`auth__form__input ${
+              displayFullnameError && "focus:border-error"
+            }`}
+          />
+        </div>
+
+        {/* Username Input */}
+        <div className="flex flex-col gap-1">
+          <div className="flex items-center justify-between">
+            <FormLabel htmlFor="username" label="Username" variant="regular" />
+
+            {displayUsernameError && (
+              <FormLabel
+                label={
+                  displayUsernameError ? (formik.errors.username as string) : ""
+                }
+                variant="error"
+              />
+            )}
+          </div>
+
+          <input
+            required
+            type="text"
+            name="username"
+            value={formik.values.username}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className={`auth__form__input ${
+              displayUsernameError && "focus:border-error"
+            }`}
+          />
+        </div>
+
+        {/* Email Input */}
+        <div className="flex flex-col gap-1">
+          <div className="flex items-center justify-between">
+            <FormLabel htmlFor="email" label="Email" variant="regular" />
+
+            {displayEmailError && (
+              <FormLabel
+                label={displayEmailError ? (formik.errors.email as string) : ""}
+                variant="error"
+              />
+            )}
+          </div>
+
+          <input
+            required
+            type="email"
+            name="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className={`auth__form__input ${
+              displayEmailError && "focus:border-error"
+            }`}
+          />
+        </div>
+
+        {/* Submit Button */}
+        <button
+          type="submit"
+          disabled={loading}
+          className="w-max text-text3 bg-primary hover:bg-[#3446E5] active:bg-[#2E3ECC]"
+        >
+          {loading ? "Loading..." : "Update"}
+        </button>
+      </form>
+    </section>
   );
 }
 

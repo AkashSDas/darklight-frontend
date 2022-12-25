a452e0bf5782e4fbb07cf93699d332f688680b7c
AkashSDas <aakashdas368@gmail.com>
2022-12-25 19:07:24 +0530

    refactor settings

--- pages/courses/[courseId]/settings.tsx
+++ pages/courses/[courseId]/settings.tsx
@@ -1,46 +1,18 @@
 import { ReactElement } from "react";
 
-import CourseEditorLayout from "../../../components/course-editor/layout";
-import Banner from "../../../components/course-settings/banner";
-import DeleteCourse from "../../../components/course-settings/delete-course";
-import DescriptionInput from "../../../components/course-settings/description";
-import DifficultyLevelInput from "../../../components/course-settings/difficulty-input";
-import PriceInput from "../../../components/course-settings/price-input";
-import PublishCourse from "../../../components/course-settings/publish-course";
-import CourseTags from "../../../components/course-settings/tags-input";
-import TitleInput from "../../../components/course-settings/title";
-import { useEditableCourse } from "../../../lib/hooks.lib";
+import Banner from "@components/course-editor-settings/banner";
+import CourseEditorLayout from "@components/shared/course-editor-layout";
 
-function CourseSettingsPage() {
-  var { loading, course, error } = useEditableCourse();
-
-  if (loading) return <div>Loading...</div>;
-
+export default function CourseSettingsPage(): JSX.Element {
   return (
-    <main className="mt-4 w-full flex flex-col gap-2 items-center">
-      <section className="w-full max-w-[800px] flex flex-col items-center gap-2">
+    <main className="w-full flex flex-col gap-2 items-center">
+      <div className="w-full max-w-[800px] flex flex-col gap-2 items-center">
         <Banner />
-        <TitleInput />
-        <DescriptionInput />
-        <hr className="my-4 bg-border opacity-50 w-full" />
-        <CourseTags />
-        <hr className="my-4 bg-border opacity-50 w-full" />
-        <DifficultyLevelInput />
-        <hr className="my-4 bg-border opacity-50 w-full" />
-        <PriceInput />
-        <hr className="my-4 bg-border opacity-50 w-full" />
-        <PublishCourse />
-        <hr className="my-4 bg-border opacity-50 w-full" />
-        <DeleteCourse />
-
-        <div className="my-8"></div>
-      </section>
+      </div>
     </main>
   );
 }
 
 CourseSettingsPage.getLayout = function getLayout(page: ReactElement) {
-  return <CourseEditorLayout>{page}</CourseEditorLayout>;
+  return <CourseEditorLayout context="course">{page}</CourseEditorLayout>;
 };
-
-export default CourseSettingsPage;

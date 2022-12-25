d49113599a56cfe849b6ef8390859490fb45555b
AkashSDas <aakashdas368@gmail.com>
2022-12-25 13:57:06 +0530

    add course editor layout

---
+++ components/shared/course-editor-layout.tsx
@@ -0,0 +1,50 @@
+import { useRouter } from "next/router";
+
+import CourseEditorSidebar from "@components/shared/course-editor-sidebar";
+import { useEditableCourse, useEditableGroup, useEditableLesson, useUser } from "@lib/hooks.lib";
+
+import CourseEditorNavbar from "./course-editor-navbar";
+
+interface Props {
+  children: JSX.Element | JSX.Element[] | string | string[] | null;
+  context: "course" | "group" | "lesson";
+}
+
+export default function CourseEditorLayout(props: Props): JSX.Element {
+  var { course } = useEditableCourse();
+  var { group, courseLoading } = useEditableGroup();
+  var { lesson, loading: lessonLoading } = useEditableLesson();
+  var router = useRouter();
+  var { user } = useUser();
+
+  function DisplayContent(): JSX.Element {
+    if (!user) return <div>Unauthorized</div>;
+
+    if (props.context == "lesson" && router.query?.lessonId) {
+      if (lesson) return <>{props.children}</>;
+      else if (lessonLoading) return <div>Loading...</div>;
+      else return <div>Lesson not found</div>;
+    } else if (props.context == "group" && router.query?.groupId) {
+      if (group) return <>{props.children}</>;
+      else if (courseLoading) return <div>Loading...</div>;
+      else return <div>Group not found</div>;
+    } else if (props.context == "course" && router.query?.courseId) {
+      if (course) return <>{props.children}</>;
+      else if (courseLoading) return <div>Loading...</div>;
+      else return <div>Course not found</div>;
+    }
+
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="flex font-urbanist font-medium">
+      <CourseEditorSidebar />
+
+      <div className="ml-[300px] w-full">
+        <CourseEditorNavbar />
+        <DisplayContent />
+      </div>
+    </div>
+  );
+}

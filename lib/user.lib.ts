a29500c8077061e6213c9b5d926e0349df907ca8
AkashSDas <aakashdas368@gmail.com>
2023-01-05 19:58:51 +0530

    fix emoji mart import and refactor user util

--- lib/user.lib.ts
+++ lib/user.lib.ts
@@ -1,18 +1,3 @@
-import { User } from "../store/user/slice";
-
-export function normalizeJsonToUser(user: any): User {
-  return {
-    username: user?.username ?? undefined,
-    email: user?.email ?? undefined,
-    active: user?.active ?? false,
-    verified: user?.verified ?? false,
-    roles: user?.roles ?? ["student"],
-    oauthProviders: user?.oauthProviders ?? [],
-    id: user._id ?? null,
-    createdAt: new Date(user.createdAt).toISOString(),
-  };
-}
-
 export interface UpdateDetailsInput {
   username: string;
   email: string;
